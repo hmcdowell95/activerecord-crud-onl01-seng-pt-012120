d61125f08702777ce5a92139accecc5dd02d39c0
Harley McDowell <hmcdowell95@gmail.com>
2020-04-05 23:17:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,10 +27,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
+  a = title
+  b = release_date
   Movie.create do |m|
-    m.title = args[title]
-    m.release_date = args[release_date]
+    m.title = a
+    m.release_date = b
   end
 end
 
